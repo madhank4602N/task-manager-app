73009d4ff0fcf7315a72293620eff47ef6c2b157
madhank4602N <mmadhankumarkm@gmail.com>
2026-02-15 22:37:55 +0530

    first commit

--- frontend/src/pages/TaskEdit.jsx
+++ frontend/src/pages/TaskEdit.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { fetchTaskByIdApi, updateTaskApi } from "../api/taskApi";
 import TaskForm from "../components/TaskForm";
 import { useAuth } from "../context/AuthContext";
